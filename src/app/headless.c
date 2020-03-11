b44b9a2694aaf6890732a054938d2e449bb4db8e
Uri <rotshtein@gmail.com>
2020-03-11 11:48:48 +0200

    VERSION 1.1

--- src/app/headless.c
+++ src/app/headless.c
@@ -76,7 +76,7 @@
 /******************************************************************************/
 extern ad9528Device_t clockAD9528_;
 extern mykonosDevice_t mykDevice;
-#define VERSION "1.0"
+#define VERSION "1.1"
 
 
 void print_help()
